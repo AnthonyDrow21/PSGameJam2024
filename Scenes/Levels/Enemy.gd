70a23a23e08d7c76f35e4c40763a1c2f1297f676
surfsidegryphon <surfsidegryphon@gmail.com>
2023-12-27 14:19:44 -0500

    Enemy Scene, Enemy AI, Map Shift
    The first enemy scene was created with first pass at AI.  Shifted the map 1 pixel to the left because it was bugging me.

--- Scenes/Levels/Enemy.gd
+++ Scenes/Levels/Enemy.gd
@@ -1,11 +1,44 @@
-extends Node2D
+extends CharacterBody2D
 
+var _speed = 30
 
-# Called when the node enters the scene tree for the first time.
+##var velocity = Vector2.ZERO
+
+var _player
+var _randomnum
+var _target
+
+enum {
+	SURROUND,
+	ATTACK,
+	HIT
+}
+
+var _state = SURROUND
+
 func _ready():
-	pass # Replace with function body.
+	var _rng = RandomNumberGenerator.new()
+	_rng.randomize()
+	_randomnum = _rng.randf()
+	_target = get_circle_position(_randomnum)
 
+func _physics_process(delta):
+	match _state:
+		SURROUND:
+			move(_target,delta)
+			
+func move(target,delta):
+	var _direction = (target - global_position).normalized()
+	var desired_velocity = _direction * _speed 
+	var _steering = (desired_velocity - velocity) * delta * 2.5
+	velocity += _steering
+	move_and_slide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func get_circle_position(random):
+	var kill_circle_center = get_local_mouse_position() ##Switch out with player location later
+	var _radius = 40
+	var _angle = random * PI * 2;
+	var x = kill_circle_center.x + cos(_angle) * _radius;
+	var y = kill_circle_center.y + sin(_angle) * _radius;
+	
+	return Vector2(x, y)
