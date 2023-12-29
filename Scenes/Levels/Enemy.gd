8a81fe7e7002979de8974ca1e03df5b33392d382
AnthonyDrow <anthonyedrow@gmail.com>
2023-12-29 15:05:33 -0500

    Updated enemies to follow player.

--- Scenes/Levels/Enemy.gd
+++ Scenes/Levels/Enemy.gd
@@ -4,7 +4,8 @@ var _speed = 30
 
 ##var velocity = Vector2.ZERO
 
-var _player
+# @onready means that this variable will be instantiated after the ready() function is called.
+@onready var player = get_tree().get_nodes_in_group("Player")[0];
 var _randomnum
 var _target
 
@@ -20,9 +21,10 @@ func _ready():
 	var _rng = RandomNumberGenerator.new()
 	_rng.randomize()
 	_randomnum = _rng.randf()
-	_target = get_circle_position(_randomnum)
 
 func _physics_process(delta):
+	_target = get_circle_position(_randomnum)
+	
 	match _state:
 		SURROUND:
 			move(_target,delta)
@@ -35,7 +37,9 @@ func move(target,delta):
 	move_and_slide()
 
 func get_circle_position(random):
-	var kill_circle_center = get_local_mouse_position() ##Switch out with player location later
+	var kill_circle_center = player.position ##Switch out with player location later
+	print("current kill center: ", kill_circle_center)
+	
 	var _radius = 40
 	var _angle = random * PI * 2;
 	var x = kill_circle_center.x + cos(_angle) * _radius;
