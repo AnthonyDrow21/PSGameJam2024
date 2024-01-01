adab491d52efa2f7590c57af98f8ef4735422f75
AnthonyDrow <anthonyedrow@gmail.com>
2024-01-01 13:25:57 -0500

    Fixed bullet direction targetting.

--- Scenes/Levels/Main1.gd
+++ Scenes/Levels/Main1.gd
@@ -13,7 +13,9 @@ func _process(delta: float) -> void:
 
 func _on_player_magic_wand_shoot(bullet: Variant, direction: Variant, location: Variant) -> void:
 	var spawnedBullet = bullet.instantiate();
-	add_child(spawnedBullet);
-	spawnedBullet.rotation = direction;
 	spawnedBullet.position = location;
+	## AED ## We need to set the bullet's position first before we add it to the scene, otherwise the 
+	# ready function will think the bullet's position is zero rather than the player's position.
+	add_child(spawnedBullet);
+	#spawnedBullet.rotation = direction;
 	#spawnedBullet.velocity = spawnedBullet.velocity.rotated(direction);
--- Scenes/Weapons/MagicWandBullet.gd
+++ Scenes/Weapons/MagicWandBullet.gd
@@ -4,19 +4,22 @@ extends Area2D
 @onready var enemies = get_tree().get_nodes_in_group("Enemy");
 var bulletSpeed = 100.0;
 var targetVector = Vector2.RIGHT;
+var closestEnemy;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Find the nearest enemy when we enter the scene and then save that as our velocity vector.
 	var closestDistance = 1000000;
-	var closestEnemy;
 	for enemy in enemies:
+		#print(self.position);
 		var distance = position.distance_to(enemy.position);
+		#print(distance);
 		if(distance < closestDistance):
 			closestDistance = distance;
 			closestEnemy = enemy;
 	
-	targetVector = closestEnemy.position.direction_to(self.position);
+	targetVector = self.position.direction_to(closestEnemy.position);
+	print(targetVector);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
