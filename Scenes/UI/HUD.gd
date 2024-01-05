7cce714123e5c566e558c5dcfa2ea9a000ba162d
AnthonyDrow <anthonyedrow@gmail.com>
2024-01-04 19:33:15 -0500

    Added HUD to main scene that displays current timer count down.

---
+++ Scenes/UI/HUD.gd
@@ -0,0 +1,26 @@
+class_name HUD;
+extends CanvasLayer;
+
+# Max time in seconds
+var maxTime = 1800.0;
+
+var currentTime: float = 0.0;
+var minutesLeft: int = 0;
+var secondsLeft: int = 0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	minutesLeft = (maxTime - currentTime) / 60;
+	secondsLeft = fmod(maxTime, 60);
+	$Timer.text = "%02d:%02d" % [minutesLeft, secondsLeft];
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	currentTime += delta;
+	#var minutes = fmod(currentTime, 3600) / 60;
+	minutesLeft = (maxTime - currentTime) / 60;
+	secondsLeft = fmod(maxTime - currentTime, 60);
+	$Timer.text = "%02d:%02d" % [minutesLeft, secondsLeft];
+	pass
