9d2a8ddcd1f71281be0ebd712f4b7195295bef40
SurfsideRaven <smithr.2428@gmail.com>
2024-01-09 18:57:19 -0500

    Adding Damage Node
    When enemies are hit, the damage is displayed on the screen.

--- Scenes/NPC/Enemy.gd
+++ Scenes/NPC/Enemy.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var enemyHealth = 5
 @onready var player = get_tree().get_first_node_in_group("Player")
 @onready var incomingProjectile
+const EnemyDMGAnim = preload("res://Scenes/NPC/EnemyDMG.tscn")
 var incomingDamage = 1
 var xpValue = 1.0;
 
@@ -14,7 +15,20 @@ func _physics_process(_delta):
 	move_and_slide()
 
 func DamageEnemy(damage):
+	Display_DMG(damage)
 	enemyHealth -= damage;
 	if enemyHealth <= 0:
 		player.gainXp(xpValue);
 		queue_free()
+
+func damage_effect(EFFECT: PackedScene, effect_position: Vector2 = global_position):
+	if EFFECT:
+		var effect = EFFECT.instantiate()
+		get_tree().current_scene.add_child(effect)
+		effect.global_position = effect_position
+		return effect
+
+func Display_DMG(damage: int):
+	var indicator = damage_effect(EnemyDMGAnim)
+	if indicator:
+		indicator.label.text = str(damage)
